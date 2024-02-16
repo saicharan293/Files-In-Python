8575a63ea3e4a6049b8fdc15f6c4783a0c78ef53
saicharan293 <2493saicharan@gmail.com>
2024-02-16 10:05:37 +0530

    copy files, folders from src to dst
    Copy files, folders from one file/folder to another file/folder

---
+++ Working_With_Files/Copying_Files.py
@@ -0,0 +1,14 @@
+import shutil
+
+def copy_file(src,dest):
+    shutil.copy(src,dest)
+
+def copy_folder(src,dest):
+    shutil.copytree(src,dest)
+
+source='./files/02/demos/files/02_file.txt'
+destination='./files/02/demos/files/subfolder'
+source_folder='./files/02/demos/files/subfolder'
+destination_folder='./files/02/demos/files/newfolder'
+# copy_file(source,destination)
+copy_folder(source_folder,destination_folder)
